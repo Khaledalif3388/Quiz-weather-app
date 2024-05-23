e4ba26590e0c7e6ac2347e364494f7e0792d0f51
Md. Khaled Hasan (Alif) <98605014+Khaledalif3388@users.noreply.github.com>
2024-05-23 17:55:21 +0600

    quit+weather

---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+// main.dart
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'personal_info_provider.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'Apps.dart';
+import 'package:weather_app/login.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => PersonalInfoProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const LoginPage(),
+      theme: ThemeData(
+        primaryColor: Colors.black87,
+        hintColor: Colors.black87,
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+          titleMedium: TextStyle(
+            fontSize: 20,
+            color: Colors.black87,
+          ),
+          titleSmall: TextStyle(
+            fontSize: 18,
+            color: Colors.black54,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 7,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Khaled Hasan",
+            style: Theme.of(context).textTheme.titleLarge!,
+          ),
+          backgroundColor: Theme.of(context).primaryColor,
+          bottom: const TabBar(
+            isScrollable: true,
+            tabs: [
+              Tab(
+                icon:
+                    Icon(Icons.apps_outage_outlined, color: Colors.tealAccent),
+                text: 'Apps',
+              ),
+            ],
+          ),
+        ),
+        drawer: Drawer(
+          child: Container(
+            color: Colors.black54,
+            child: ListView(
+              padding: EdgeInsets.zero,
+            ),
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            Apps(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw "Could not launch $url";
+    }
+  }
+}
